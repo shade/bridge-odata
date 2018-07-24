87e67825f52dfc4dc3b47cad0b56998c5961da37
Joe <joetho@zillowgroup.com>
2018-07-24 10:54:45 -0700

    going to push

--- test/modifiers.js
+++ test/modifiers.js
@@ -11,16 +11,34 @@ describe('modifiers', () => {
 
     assert.equal(f.toString(), 'second(a)', 'second doesn\'t work')
   })
+
   it('should work nestedly', () => {
     const f = new filter({
       left: {
         value: 'a',
         modifier: 'sec'
       },
-      comparator: 'eq',
+      operation: 'eq',
       right: 'b'
     })
 
     assert.equal(f.toString(), 'second(a) eq b', 'second doesn')
+  })
+
+  it('should work nestedly layer 2', () => {
+    const f = new filter({
+      left: {
+        left: 'c',
+        operation: 'eq',
+        right: {
+          value: 'a',
+          modifier: 'hour'
+        }
+      },
+      operation: 'and',
+      right: 'b'
+    })
+
+    assert.equal(f.toString(), '(c eq hour(a)) and b', 'second layer nested modifier doesnt work')
   })
 })
