6b71a5097efc5326ddc222a959c20b7dcf4ae4c5
Joe <joetho@zillowgroup.com>
2018-07-27 15:19:04 -0700

    Update lambda

--- test/lambdas.js
+++ test/lambdas.js
@@ -41,4 +41,33 @@ describe('Try out any/all lambdas', () => {
 
     assert.equal(f.toString(), 'people/all(v: lol)')
   })
+  it ('Should work nested.', () => {
+    // Find all the documents where there's a person who's names start with John and have at least one child taller than 170.
+    let f = new Filter({
+      left: 'People',
+      operation: 'any',
+      variable: 'a',
+      inner: {
+        left: 'a/Children',
+        operation: 'all',
+        variable: 'b',
+        inner: {
+          left: {
+            left: 'a/Name',
+            operation: 'startswith',
+            right: '"John"'
+          },
+          operation: 'and',
+          right: {
+            left: 'b/Height',
+            operation: 'ge',
+            right: '170'
+          }
+        }
+      }
+    })
+
+    assert.equal(f.toString(), 'People/any(a: a/Children/all(b: (startswith(a/Name,"John")) and (b/Height ge 170)))')
+
+  })
 })
